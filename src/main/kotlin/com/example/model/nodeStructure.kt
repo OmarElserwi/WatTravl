723f0b8c14b46e0899e64a327497b1c842ad910c
OmarElserwi <omar.elserwy@hotmail.com>
2024-06-16 10:16:55 -0400

    adding node structure

---
+++ src/main/kotlin/com/example/model/nodeStructure.kt
@@ -0,0 +1,13 @@
+data class HallwayNode(
+    val nodeId: Int,                        // Identifier for the hallway node
+    var north: HallwayNode? = null,            // North neighboring hallway node
+    var south: HallwayNode? = null,            // South neighboring hallway node
+    var east: HallwayNode? = null,             // East neighboring hallway node
+    var west: HallwayNode? = null,             // West neighboring hallway node
+    val classrooms: MutableList<ClassroomNode> = mutableListOf() // Connected classroom nodes
+)
+
+data class ClassroomNode(
+    val roomId: Int,       // Identifier for the classroom
+    var connectedHallway: HallwayNode? = null // Connected hallway node
+)
