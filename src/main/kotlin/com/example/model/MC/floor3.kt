80e39b084b92ccacbe4b2750d2ca927b761e1689
OmarElserwi <omar.elserwy@hotmail.com>
2024-06-30 18:27:37 -0400

    adding edges for floor 3

--- src/main/kotlin/com/example/model/MC/floor3.kt
+++ src/main/kotlin/com/example/model/MC/floor3.kt
@@ -1,22 +1,58 @@
-fun createHallways(): Map<Int, HallwayNode> {
-    return (301..335).associateWith { HallwayNode(nodeId = it) }
+fun buildGraph(): Map<Int, HallwayNode> {
+    println("Classrooms created")
+
+    // Create hallway nodes
+    val hallways = createHallways()
+
+    println("Hallways created")
+
+    // Connect hallways
+    connectHallways(hallways)
+
+    println("Hallways connected")
+
+    // Set distances between hallways in the global distances map
+    setDistances(hallways)
+
+    println("Distances set")
+
+    // Connect classrooms to hallways
+    connectClassroomsToHallways(hallways)
+
+    println("Classrooms connected")
+
+    
+
+
+    return hallways
 }
+
+fun createHallways(): Map<Int, HallwayNode> {
+    return (301..335).associateWith { HallwayNode(nodeId = it) } +
+           additionalHallways.associateWith { HallwayNode(nodeId = it) }
+}
+
 
 fun connectClassroomsToHallways(hallways: Map<Int, HallwayNode>) {
     hallways[301]?.classrooms?.addAll(listOf(30121, 3012, 3072, 3089))
-    hallways[302]?.classrooms?.addAll(listOf(3011, 30901, 3071))
+    hallways[302]?.classrooms?.addAll(listOf(3011, 30901, 3009, 3071))
+    hallways[303]?.classrooms?.addAll(listOf(3009, 3115))
+    hallways[304]?.classrooms?.addAll(listOf(3115))
     hallways[305]?.classrooms?.addAll(listOf(30151, 3016, 3091))
     hallways[306]?.classrooms?.add(3091)
     hallways[307]?.classrooms?.addAll(listOf(3074, 3093, 3094))
-    hallways[308]?.classrooms?.add(3017)
+    hallways[308]?.classrooms?.add(listof(3017, 3018))
     hallways[309]?.classrooms?.addAll(listOf(3068, 3066, 3067, 3065))
     hallways[310]?.classrooms?.addAll(listOf(3010, 30091))
-    hallways[311]?.classrooms?.addAll(listOf(301811, 302210, 30))
+    hallways[311]?.classrooms?.addAll(listOf(3018, 3022, 3061 3026))
     hallways[312]?.classrooms?.addAll(listOf(3062, 3064))
     hallways[313]?.classrooms?.addAll(listOf(3060, 3059, 3058))
+    hallways[314]?.classrooms?.addAll(listOf(3008, 3007, 3006, 3005))
+    hallways[315]?.classrooms?.addAll(listOf(3022, 3026, 3027))
     hallways[316]?.classrooms?.addAll(listOf(3055, 3054, 3053, 3056))
     hallways[317]?.classrooms?.addAll(listOf(3029, 3030))
     hallways[318]?.classrooms?.addAll(listOf(3051, 3052, 3050))
+    hallways[319]?.classrooms?.addAll(listOf(3006, 3004, 3005, 3003))
     hallways[320]?.classrooms?.addAll(listOf(3031, 3033))
     hallways[321]?.classrooms?.addAll(listOf(3047, 3048, 3045, 3046))
     hallways[322]?.classrooms?.addAll(listOf(3044, 3043))
@@ -28,6 +64,161 @@ fun connectClassroomsToHallways(hallways: Map<Int, HallwayNode>) {
     hallways[328]?.classrooms?.add(3080)
     hallways[329]?.classrooms?.add(30842)
     hallways[330]?.classrooms?.add(3070)
+    hallways[331]?.classrooms?.add(3001)
+    hallways[332]?.classrooms?.add(3001)
     hallways[333]?.classrooms?.add(3096)
-    hallways[335]?.classrooms?.addAll(listOf(3075, 3095))
+    hallways[334]?.classrooms?.add(3002)
+    hallways[335]?.classrooms?.addAll(listOf(3075, 3095, 3002))
 }
+
+
+fun connectHallways(hallways: Map<Int, HallwayNode>) {
+    hallways[301]?.apply {
+        south = hallways[302]
+
+    }
+    hallways[302]?.apply {
+        north = hallways[301]
+        east = hallways[303]
+        south = hallways[310]
+    }
+    hallways[303]?.apply {
+        east = hallways[304]
+        west = hallways[302]
+    }
+    hallways[304]?.apply {
+        east = hallways[303]
+        west = hallways[305]
+        south = hallways[308]
+    }
+    hallways[305]?.apply {
+        south = hallways[309]
+        east = hallways[304]
+        west = hallways[306]
+    }
+    hallways[306]?.apply {
+        east = hallways[305]
+        west = hallways[307]
+    }
+    hallways[307]?.apply {
+        west = hallways[306]
+    }
+    hallways[308]?.apply {
+        north = hallways[304]
+        south = hallways[311]
+    }
+    hallways[309]?.apply {
+        north = hallways[305]
+        south = hallways[312]
+    }
+    hallways[310]?.apply {
+        north = hallways[302]
+        south = hallways[314]
+    }
+    hallways[311]?.apply {
+        north = hallways[308]
+        south = hallways[315]
+    }
+    hallways[312]?.apply {
+        north = hallways[309]
+        south = hallways[313]
+    }
+    hallways[313]?.apply {
+        north = hallways[312]
+        south = hallways[316]
+    }
+    hallways[314]?.apply {
+        north = hallways[310]
+        south = hallways[319]
+    }
+    hallways[315]?.apply {
+        north = hallways[311]
+        south = hallways[317]
+    }
+    hallways[316]?.apply {
+        north = hallways[313]
+        south = hallways[318]
+    }
+    hallways[317]?.apply {
+        north = hallways[315]
+        south = hallways[320]
+        east = hallways[XXX]
+        west = hallways[XXX]
+    }
+    hallways[318]?.apply {
+        north = hallways[316]
+        south = hallways[321]
+    }
+    hallways[319]?.apply {
+        north = hallways[314]
+        south = hallways[331]
+        east = hallways[XXX]
+        west = hallways[XXX]
+    }
+    hallways[320]?.apply {
+        north = hallways[317]
+        south = hallways[323]
+        east = hallways[XXX]
+        west = hallways[XXX]
+    }
+    hallways[321]?.apply {
+        north = hallways[318]
+        south = hallways[322]
+    }
+    hallways[322]?.apply {
+        north = hallways[321]
+        south = hallways[324]
+    }
+    hallways[323]?.apply {
+        north = hallways[320]
+        south = hallways[325]
+    }
+    hallways[324]?.apply {
+        north = hallways[322]
+        south = hallways[326]
+    }
+    hallways[325]?.apply {
+        north = hallways[323]
+        south = hallways[339]
+    }
+    hallways[326]?.apply {
+        north = hallways[324]
+        south = hallways[335]
+    }
+    hallways[327]?.apply {
+        east = hallways[328]
+    }
+    hallways[328]?.apply {
+        west = hallways[327]
+        east = hallways[329]
+    }
+    hallways[329]?.apply {
+        east = hallways[328]
+        west = hallways[330]
+    }
+    hallways[330]?.apply {
+        east = hallways[331]
+        west = hallways[329]
+    }
+    hallways[331]?.apply {
+        east = hallways[332]
+        west = hallways[330]
+    }
+    hallways[332]?.apply {
+        east = hallways[333]
+        west = hallways[331]
+    }
+    hallways[333]?.apply {
+        east = hallways[334]
+        west = hallways[332]
+    }
+    hallways[334]?.apply {
+        east = hallways[335]
+        west = hallways[333]
+    }
+    hallways[335]?.apply {
+        north = hallways[326]
+        west = hallways[334]
+    }
+}
+
